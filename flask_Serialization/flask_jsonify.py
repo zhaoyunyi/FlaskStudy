e424b4e984e5637182611cb3cea9d3d5fb18716f
zhaoyunyi <zcloudswing@gmail.com>
2018-08-09 22:03:14 +0800

    flask_dumps
    python_dumps 和 flask jsonfy 之间的对比

--- flask_Serialization/flask_jsonify.py
+++ flask_Serialization/flask_jsonify.py
@@ -1,15 +1,26 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from flask import jsonify
-from flask import Response
+from flask import json
+
 
 app = Flask(__name__)
 
 
-@app.route('/getjsonstr', methods=['GET'])
+@app.route('/getjsonify', methods=['GET'])
 def get_json():
     data = [{'a': 1, 'b': 2}, {'c': 3, 'd': 4}]
     return jsonify(result=data)
+# jsonify 可以传入对象为参数，并且支持多参对象转json，在head 里的返回 content-type →application/json
+# 会在body 里返回对应的json 对象
+
+
+@app.route('/getdumps', methods=['GET'])
+def getdumps():
+    data = [{'a': 1, 'b': 2}, {'c': 3, 'd': 4}]
+    return json.dumps(data)
+# python 自带的dumps 输出的是html/text 文本，并不是json类型的数据，并且不支持多参
+# content-type →text/html; charset=utf-8
 
 
 if __name__ == '__main__':
